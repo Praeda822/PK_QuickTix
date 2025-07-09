9d933dabb4da4f553b8218b2c9e8839cee7dc0b8
Praeda822 <patdkelly5@gmail.com>
2025-07-09 14:57:11 +1000

    Exception capture via Sentry

--- quick-tix/src/actions/ticket.actions.ts
+++ quick-tix/src/actions/ticket.actions.ts
@@ -13,23 +13,31 @@ export async function createTicket(
   // the success property is a boolean indicating if the action was successful
   // the message property is a string that will be displayed to the user
 ): Promise<{ success: boolean; message: string }> {
-  const subject = formData.get('subject') as string;
-  const description = formData.get('description') as string;
-  const priority = formData.get('priority') as string;
+  try {
+    throw new Error('This is a test error for Prisma'); // Test error to verify Sentry integration
 
-  // Error validation and error catching for the formData using Sentry
-  // If any of the fields are missing, return an error message and, as a second argument, log a warning to Sentry
-  if (!subject || !description || !priority) {
-    Sentry.captureMessage('Validation Failed: Missing ticket fields', 'warning');
+    const subject = formData.get('subject') as string;
+    const description = formData.get('description') as string;
+    const priority = formData.get('priority') as string;
+
+    // Error validation and error catching for the formData using Sentry
+    // If any of the fields are missing, return an error message and, as a second argument, log a warning to Sentry
+    if (!subject || !description || !priority) {
+      Sentry.captureMessage('Validation Failed: Missing ticket fields', 'warning');
+      return {
+        success: false,
+        message: 'All fields are required.',
+      };
+    }
+    // Here I would typically send the data to my backend API
+    return { success: true, message: 'Ticket created successfully' };
+  } catch (error) {
+    Sentry.captureException(error as Error, {
+      extra: { formData: Object.fromEntries(formData.entries()) },
+    });
     return {
       success: false,
-      message: 'All fields are required.',
+      message: 'An error has occured while creating your ticket.',
     };
   }
-
-  // Here I would typically send the data to my backend API
-  // I'll just log it to console for now..
-  console.log(subject, description, priority);
-
-  return { success: true, message: 'Ticket created successfully' };
 }
