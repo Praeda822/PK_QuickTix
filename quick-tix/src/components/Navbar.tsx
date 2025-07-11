695310da75e7fe079026c876835e16a83eaaa63c
Praeda822 <patdkelly5@gmail.com>
2025-07-11 16:26:23 +1000

    configured logic to conditionally render Navbar links whether a user is logged in or not

--- quick-tix/src/components/Navbar.tsx
+++ quick-tix/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
+import { getCurrentUser } from '../lib/current-user';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await getCurrentUser();
   return (
     <nav className="bg-white border-b border-gray-200 px-6 py-4 flex justify-between items-center">
       <div>
@@ -9,26 +11,35 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center space-x-4">
-        <Link href="/tickets/new" className="text-gray-700 hover:underline transition">
-          New Ticket
-        </Link>
-        <Link href="/tickets" className="text-gray-700 hover:underline transition">
-          My Tickets
-        </Link>
-        <Link href="/login" className="text-blue-600 hover:underline transition">
-          Login
-        </Link>
-        <Link
-          href="/register"
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
-        >
-          Register
-        </Link>
-        <form>
-          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">
-            Logout
-          </button>
-        </form>
+        {user ? (
+          <>
+            {' '}
+            <Link href="/tickets/new" className="text-gray-700 hover:underline transition">
+              New Ticket
+            </Link>
+            <Link href="/tickets" className="text-gray-700 hover:underline transition">
+              My Tickets
+            </Link>
+            <form>
+              <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">
+                Logout
+              </button>
+            </form>
+          </>
+        ) : (
+          <>
+            {' '}
+            <Link href="/login" className="text-blue-600 hover:underline transition">
+              Login
+            </Link>
+            <Link
+              href="/register"
+              className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+            >
+              Register
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
